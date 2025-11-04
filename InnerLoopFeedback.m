f0cdbf15c8289817b5b079e4d05eba4e45e9b653
ianjaggi <iaja6607@colorado.edu>
2025-11-04 13:28:34 -0700

    Update InnerLoopFeedback.m

--- InnerLoopFeedback.m
+++ InnerLoopFeedback.m
@@ -18,9 +18,10 @@ function [Fc, Gc] = InnerLoopFeedback(var)
     Lc = -0.00116*phi - 0.000696*p;
     Mc = -0.00144*theta - 0.000864*q;
     Nc = -0.004*r;
-    Fc = [0 0 m*g/(cos(phi)*cos(theta))]';
+    Fc = [0 0 -m*g/(cos(phi)*cos(theta))]';
     Gc = [Lc Mc Nc]';
 
 
 
 end
+
