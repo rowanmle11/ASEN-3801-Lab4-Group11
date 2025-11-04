46413b66d5d45443e4dcc2b6f6c78f2431f17cfd
AntonioJF1 <anji9940@colorado.edu>
2025-11-04 13:09:10 -0700

    Add files via upload

---
+++ InnerLoopFeedback.m
@@ -0,0 +1,25 @@
+function [Fc, Gc] = InnerLoopFeedback(var)
+
+    xE = var(1);
+    yE = var(2);
+    zE = var(3);
+    phi = var(4);
+    theta = var(5);
+    psi = var(6);
+    uEB = var(7);
+    vEB = var(8);
+    wEB = var(9);
+    p = var(10);
+    q = var(11);
+    r = var(12);
+
+    m = .068;
+    g = 9.81;
+    Lc = 0;
+    Mc = 0;
+    Nc = 0;
+    Fc = [0 0 m*g/(cos(phi)*cos(theta))]';
+    Gc = [Lc Mc Nc]';
+
+
+end
